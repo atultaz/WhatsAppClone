c4ac82fafed1aa205f667282970f2a7f4498711d
atultaz <atul.tiwari@me.com>
2021-12-20 19:53:47 +0530

    fetch and display message

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,32 +1,52 @@
 import React, { useEffect, useState } from 'react';
-import {
-  FlatList,
-  Text,
-  ImageBackground,
-  KeyboardAvoidingView,
-} from 'react-native';
-
-import { API, graphqlOperation, Auth } from 'aws-amplify';
+import { FlatList, Text, ImageBackground } from 'react-native';
 
 import { useRoute } from '@react-navigation/native';
+import { API, graphqlOperation, Auth } from 'aws-amplify';
 
-import chatRoomData from '../data/Chats';
+import { messagesByChatRoom } from '../src/graphql/queries';
+
 import ChatMessage from '../components/ChatMessage';
 import BG from '../assets/images/BG.png';
 import InputBox from '../components/InputBox';
 
 const ChatRoomScreen = () => {
-  const route = useRoute();
+  const [messages, setMessages] = useState([]);
+  const [myId, setMyId] = useState(null);
 
-  console.log(route.params);
+  const route = useRoute();
+  console.log(route.params.id);
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      const messagesData = await API.graphql(
+        graphqlOperation(messagesByChatRoom, {
+          chatRoomID: route.params.id,
+          sortDirection: 'DESC',
+        })
+      );
+
+      setMessages(messagesData.data.messagesByChatRoom.items);
+    };
+    fetchMessages();
+  }, []);
+
+  useEffect(() => {
+    const getMyId = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser();
+      setMyId(userInfo.attributes.sub);
+    };
+    getMyId();
+  }, []);
 
   return (
     <ImageBackground style={{ width: '100%', height: '100%' }} source={BG}>
       <FlatList
-        data={chatRoomData.messages}
-        renderItem={({ item }) => <ChatMessage message={item} />}
+        data={messages}
+        renderItem={({ item }) => <ChatMessage myId={myId} message={item} />}
         inverted
       />
+
       <InputBox chatRoomID={route.params.id} />
     </ImageBackground>
   );
